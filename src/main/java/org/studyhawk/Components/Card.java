75cf3725db8e89998bad6f2cca78e2eef450f7e7
Tyler Magoteaux <magotetp@miamioh.edu>
2024-09-13 09:46:35 -0400

    Components for app.

---
+++ src/main/java/org/studyhawk/Components/Card.java
@@ -0,0 +1,21 @@
+public class Card{
+    private String term;
+    private String definition;
+    private String image;
+    private boolean favorite;
+    
+    public Card(String term, String definition) {
+        this.term = term;
+        this.definition = definition;
+    }
+
+    public Card(String term, String definition, String image) {
+        this.term = term;
+        this.definition = definition;
+        this.image = image;
+    }
+
+    public String toString() {
+        return term + "\n" + definition;
+    }
+}
---
+++ src/main/java/org/studyhawk/Components/Deck.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private String title;
+    private String description;
+    private boolean favorite;
+    private ArrayList<Card> cards = new ArrayList<Card>();
+
+    public Deck(String title, String description) {
+        this.title = title;
+        this.description = description;
+        favorite = false;
+    }
+
+    /** 
+     * @return boolean
+     */
+    public boolean favorite() {
+        if(favorite == false) {
+            favorite = true;
+        } else {
+            favorite = false;
+        }
+        return favorite;
+    }
+
+    
+    /** 
+     * @param newCard
+     */
+    public void addCard(Card newCard) {
+        cards.add(newCard);
+    }
+
+    public void listDeck() {
+        for (int i = 0; i < cards.size(); i++) {
+            System.out.println(cards.get(i).toString() + "\n");
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
