f9ba95b63d576e1b96ae310671f535cb7da70d8d
Austin Obringer <austin.obringer@gmail.com>
2024-10-03 16:36:24 -0400

    Remove useless imports

--- src/main/java/org/studyhawk/Application.java
+++ src/main/java/org/studyhawk/Application.java
@@ -1,17 +1,5 @@
 package org.studyhawk;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLClassLoader;
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-import java.util.Properties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
