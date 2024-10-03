428704fd879f467463e25a3f829d6fdc80e601c3
Austin Obringer <austin.obringer@gmail.com>
2024-10-03 16:27:30 -0400

    Add database structure

--- src/main/java/org/studyhawk/Application.java
+++ src/main/java/org/studyhawk/Application.java
@@ -1,5 +1,17 @@
 package org.studyhawk;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLClassLoader;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +19,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+
+		DatabaseHandler.testConnection();
 
 		SpringApplication.run(Application.class, args);
 
